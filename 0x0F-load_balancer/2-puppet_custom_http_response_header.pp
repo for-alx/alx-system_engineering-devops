9119bcff51c030c407df086bafe01dfbce455f1f
Mr-robot <hackingtest017@gmail.com>
2023-04-03 23:49:29 +0300

    :)

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,12 +1,12 @@
 # Add a custom HTTP header with Puppet
 
-exec { 'update system':
+exec { 'update':
         command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
     ensure => 'installed',
-    require => Exec['update system']
+    require => Exec['update']
 }
 
 file {'/var/www/html/index.html':
@@ -14,16 +14,16 @@ file {'/var/www/html/index.html':
 }
 
 exec {'redirect_me':
-    command => 'sed -i "24i\    rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+    command => 'sed -i "24i\    rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
     provider => 'shell'
 }
 
-exec {'HTTP header':
+exec {'header':
     command => 'sed -i "25i\    add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
     provider => 'shell'
 }
 
-service {'nginx':
+service {'restart nginx':
     ensure => running,
     require => Package['nginx']
 }
