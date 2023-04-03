616020388a3afefd2405cf6878529520dcf7139b
Mr-robot <hackingtest017@gmail.com>
2023-04-03 23:56:44 +0300

    :)

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,19 @@
 # Add a custom HTTP header with Puppet
 
-exec { 'update system':
+exec { 'apt update':
         command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
     ensure => 'installed',
-    require => Exec['update system']
+    require => Exec['apt update']
 }
 
 file {'/var/www/html/index.html':
     content => 'Hello World!'
 }
 
-exec {'redirect_me':
+exec {'redirect':
     command => 'sed -i "24i\    rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
     provider => 'shell'
 }
