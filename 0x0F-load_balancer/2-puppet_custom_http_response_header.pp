5410d8a4a8f71ac0855ef44901db30a20735e8ae
Mr-robot <hackingtest017@gmail.com>
2023-04-03 23:33:01 +0300

    :)

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -13,17 +13,17 @@ file {'/var/www/html/index.html':
     content => 'Hello World!'
 }
 
-exec {'HTTP header':
-    command  => 'sed -i "25i\    add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
-    provider => 'shell'
-}
-
-exec {'redirect_me':
+exec {'redirect':
   command  => 'sed -i "24i\  rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
   provider => 'shell'
 }
 
+exec {'HTTP header':
+    command  => 'sed -i "25i\   add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+    provider => 'shell'
+}
+
 service {'restart nginx':
-    ensure  => running,
+    ensure => running,
     require => Package['nginx']
 }
