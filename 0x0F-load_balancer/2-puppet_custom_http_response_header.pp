8202845d3e76c528bb023116e4e9d37fee00783a
Mr-robot <hackingtest017@gmail.com>
2023-04-03 23:39:12 +0300

    :)

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -13,15 +13,16 @@ file {'/var/www/html/index.html':
     content => 'Hello World!'
 }
 
+exec {'redirect':
+    command => 'sed -i "24i\    rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+    provider => 'shell'
+}
+
 exec {'header':
     command => 'sed -i "25i\    add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
     provider => 'shell'
 }
 
-exec {'redirect':
-    command => 'sed -i "24i\    rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
-    provider => 'shell'
-}
 
 service {'nginx':
     ensure => running,
