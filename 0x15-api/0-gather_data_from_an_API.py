50aee5fa80f96793c0a7cd3fa6dc7dc6645c61f1
Mr-robot <hackingtest017@gmail.com>
2023-04-24 14:54:15 +0300

    remove debuging code

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Docs
+"""
+import requests
+import sys
+
+argument = sys.argv[1]
+url = 'https://jsonplaceholder.typicode.com/'
+user = requests.get(url + 'users/{}'.format(argument)).json()
+todo = requests.get(url + 'todos?userId={}'.format(argument)).json()
+total_completed = 0
+
+for task in todo:
+    if task.get('completed') is True:
+        total_completed += 1
+
+print('Employee {} is done with tasks({}/{}):'.format(
+    user.get('name'), total_completed, len(todo)))
+
+for task in todo:
+    if task.get('completed') is True:
+        print('\t', task.get('title'))
