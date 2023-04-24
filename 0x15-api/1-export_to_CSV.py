a9cdbf08b50840d2cd68eeb43437a91d6cad7980
Mr-robot <hackingtest017@gmail.com>
2023-04-24 15:28:07 +0300

    fix style on line 18

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Export to CSV """
+import requests
+import sys
+
+if __name__ == "__main__":
+    argument = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + 'users/{}'.format(argument)).json()
+    todo = requests.get(url + 'todos?userId={}'.format(argument)).json()
+    user_status = '"{}", "{}",'.format(user.get('id'), user.get('username'))
+    csv_file_name = '{}.csv'.format(user.get('id'))
+
+    with open(csv_file_name, 'a') as f:
+        for task in todo:
+            task_status = '"{}", "{}"'.format(
+                task.get('completed'), task.get('title'))
+            print('Writing ', user_status + task_status)
+            f.write(user_status + task_status + '\n')
