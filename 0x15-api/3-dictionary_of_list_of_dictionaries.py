ed18fc996e4d1a45fa152f1b983b0cbcb0728461
Mr-robot <hackingtest017@gmail.com>
2023-04-24 16:36:54 +0300

    remove blank line at end

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Dictionary of list of dictionaries """
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    json_file_name = 'todo_all_employees.json'
+    dictionary = {}
+
+    for i in range(1, 11):
+        user = requests.get(url + 'users/{}'.format(i)).json()
+        todo = requests.get(url + 'todos?userId={}'.format(i)).json()
+        id = user.get('id')
+        singl_user = []
+        for task in todo:
+            dic = {
+                'username': user.get('username'),
+                'task': task.get('title'),
+                'completed': task.get('completed')
+            }
+            singl_user.append(dic)
+
+        dictionary[id] = singl_user
+
+    with open(json_file_name, 'w') as f:
+        json.dump(dictionary, f)
