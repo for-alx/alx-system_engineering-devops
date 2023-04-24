74a7386364c7fbdeed2384f21c851192f0d3d793
Mr-robot <hackingtest017@gmail.com>
2023-04-24 14:59:41 +0300

    remove debuging code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,19 +3,20 @@
 import requests
 import sys
 
-argument = sys.argv[1]
-url = 'https://jsonplaceholder.typicode.com/'
-user = requests.get(url + 'users/{}'.format(argument)).json()
-todo = requests.get(url + 'todos?userId={}'.format(argument)).json()
-total_completed = 0
+if __name__ == "__main__":
+    argument = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + 'users/{}'.format(argument)).json()
+    todo = requests.get(url + 'todos?userId={}'.format(argument)).json()
+    total_completed = 0
 
-for task in todo:
-    if task.get('completed') is True:
-        total_completed += 1
+    for task in todo:
+        if task.get('completed') is True:
+            total_completed += 1
 
-print('Employee {} is done with tasks({}/{}):'.format(
-    user.get('name'), total_completed, len(todo)))
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user.get('name'), total_completed, len(todo)))
 
-for task in todo:
-    if task.get('completed') is True:
-        print('\t', task.get('title'))
+    for task in todo:
+        if task.get('completed') is True:
+            print('\t', task.get('title'))
