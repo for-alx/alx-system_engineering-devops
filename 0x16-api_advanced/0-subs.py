60ce0928ad58d9fec175a0816a59d496873476f1
Mr-robot <hackingtest017@gmail.com>
2023-05-09 19:44:17 +0300

    hot fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,9 +6,11 @@ import requests
 def number_of_subscribers(subreddit):
     """ Advanced API """
 
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'},
-                     allow_redirects=False)
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
     if sub_info.status_code >= 300:
         return 0
-    return r.get('data').get('subscribers')
+
+    return sub_info.json().get("data").get("subscribers")
