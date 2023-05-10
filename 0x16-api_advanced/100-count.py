36a5d60dd6192bcd1bdd37bf177d6760c7e03e30
Mr-robot <hackingtest017@gmail.com>
2023-05-10 22:15:32 +0300

    final task

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,43 +3,34 @@
 import requests
 
 
-def count_words(subreddit, word_list, word_count={}, after=None):
+def count_words(subreddit, word_list, found_list=[], after=None):
     """ queries the Reddit API """
-    if word_count == {}:
-        word_count = {word.lower(): 0 for word in word_list}
+    sub_info = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
+                            .format(subreddit, after),
+                            headers={"User-Agent": "My-User-Agent"})
+    if after is None:
+        word_list = [word.lower() for word in word_list]
+
+    if sub_info.status_code == 200:
+        sub_info = sub_info.json()['data']
+        aft = sub_info['after']
+        sub_info = sub_info['children']
+        for post in sub_info:
+            title = post['data']['title'].lower()
+            for word in title.split(' '):
+                if word in word_list:
+                    found_list.append(word)
+        if aft is not None:
+            count_words(subreddit, word_list, found_list, aft)
+        else:
+            result = {}
+            for word in found_list:
+                if word.lower() in result.keys():
+                    result[word.lower()] += 1
+                else:
+                    result[word.lower()] = 1
+            for key, value in sorted(result.items(), key=lambda item: item[1],
+                                     reverse=True):
+                print('{}: {}'.format(key, value))
     else:
-        word_count = {k.lower(): v for k, v in word_count.items()}
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
-                            .format(subreddit),
-                            params={"after": after},
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-
-    if sub_info.status_code != 200:
-        return None
-
-    info = sub_info.json()
-
-    hot_l = [child.get("data").get("title").lower()
-             for child in info.get("data").get("children")]
-
-    for title in hot_l:
-        split_words = title.split()
-        for word in word_list:
-            if (word in split_words) and not any([
-                split_word.startswith(word) and
-                split_word[len(word):].isalnum()
-                for split_word in split_words
-            ]):
-                word_count[word.lower()] += split_words.count(word)
-
-    if not info.get("data").get("after"):
-        sorted_counts = sorted(word_count.items(),
-                               key=lambda kv: (-kv[1], kv[0]))
-        for k, v in sorted_counts:
-            if v > 0:
-                print('{}: {}'.format(k, v))
-    else:
-        return count_words(subreddit, word_list, word_count,
-                           info.get("data").get("after"))
+        return
